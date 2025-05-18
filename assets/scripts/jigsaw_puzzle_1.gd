5e4d1de450eb8d0eb02d47a6449c9a47a9cee86d
ShaShaSharma <shsharma@calpoly.edu>
2025-05-18 10:46:27 -0700

    added random menu
    random menu added no changes to spawn location yet

--- assets/scripts/jigsaw_puzzle_1.gd
+++ assets/scripts/jigsaw_puzzle_1.gd
@@ -181,7 +181,7 @@ func _input(event):
 			
 	if event is InputEventKey:
 		if event.is_pressed():
-			if event.keycode == KEY_P:
+			if event.keycode == KEY_P and (Input.is_key_pressed(KEY_SHIFT)):
 				# Arrange grid
 				arrange_grid()
 			elif event.keycode == KEY_M:
