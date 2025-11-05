dbd5287fdb3b36cb947c9362011961db2085d6a3
ntintran <147439814+ntintran@users.noreply.github.com>
2025-11-05 09:57:51 -0800

    Added back box_id setting in login.gd

--- assets/scripts/login.gd
+++ assets/scripts/login.gd
@@ -60,6 +60,7 @@ func _on_login_button_pressed():
 		file.store_line(username)
 		file.close()
 		await FireAuth.get_user_lobby(username)
+		FireAuth.box_id = username
 		
 		FireAuth.write_last_login_time() 
 		get_tree().change_scene_to_file("res://assets/scenes/nickname.tscn")
