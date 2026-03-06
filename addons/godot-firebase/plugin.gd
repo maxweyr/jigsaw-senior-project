a760e3e2f038b735b173002958a7297127b30bb8
Srinitya Doppalapudi <srinityad@gmail.com>
2026-03-05 21:26:40 -0800

    Set up build-only CI workflow

--- addons/godot-firebase/plugin.gd
+++ addons/godot-firebase/plugin.gd
@@ -1,8 +1,15 @@
 @tool
 extends EditorPlugin
 
+var _added_firebase_autoload := false
+
 func _enter_tree() -> void:
-    add_autoload_singleton("Firebase", "res://addons/godot-firebase/firebase/firebase.tscn")
+    # The project already declares Firebase in [autoload] in project.godot.
+    # Only add/remove it here if the plugin actually created it.
+    if not ProjectSettings.has_setting("autoload/Firebase"):
+        add_autoload_singleton("Firebase", "res://addons/godot-firebase/firebase/firebase.tscn")
+        _added_firebase_autoload = true
 
 func _exit_tree() -> void:
-    remove_autoload_singleton("Firebase")
+    if _added_firebase_autoload:
+        remove_autoload_singleton("Firebase")
